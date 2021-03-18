c1d87914bb01e802bf06a2760aad4d20adcc8ba3
david <daviddecastro42@live.co.uk>
2021-03-18 00:45:30 +0000

    work on header style component using material ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { flexbox } from '@material-ui/system';
+import { Container, Grid } from '@material-ui/core';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
     backgroundColor: '#fff'
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1597404294360-feeeda04612e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')`,
+    height: '500px',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: '4rem'
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
   }
 }));
 
@@ -23,6 +51,45 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog</Box>
+      </Box>
+      <Container maxWidth='lg' className={classes.blogsContainer}>
+        <Typography variant='h4' className={classes.blogTitle}>
+          Articles
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image='https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+                  title='Contemplative Reptile'
+                />
+                <CardContent>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    React useContext
+                  </Typography>
+                  <Typography
+                    variant='body2'
+                    color='textSecondary'
+                    component='p'
+                  >
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions></CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}></Grid>
+          <Grid item xs={12} sm={6} md={4}></Grid>
+          <Grid item xs={12} sm={6} md={4}></Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
